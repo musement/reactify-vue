255666715f2110346c50bc79defaacd72fcd9803
loris <lorenzo.simone@musement.com>
2019-06-14 19:54:43 +0200

    added unmount component dismission

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ export default buildVueInstance => _reactInstance => {
   const id = getId();
 
   return class Wrapper extends _reactInstance.Component {
+    constructor(props) {
+      super(props);
+      this.myself = _reactInstance.createRef();
+    }
     render() {
-      return _reactInstance.createElement("div", { id });
+      return _reactInstance.createElement("div", { ref: this.myself });
     }
     componentDidMount() {
-      const vueApp = buildVueInstance(this.props)
-      vueApp.$mount(`#${id}`);
+      this.container = document.createElement("div")
+      this.myself.current.appendChild(this.container)
+      this.vueApp = buildVueInstance(this.props)
+      this.vueApp.$mount(this.container)
     }
+    componentWillUnmount() {
+        this.vueApp.$destroy()
+        delete this.container
+        delete this.myself
+        delete this.vueApp
+    }  
   };
 };
